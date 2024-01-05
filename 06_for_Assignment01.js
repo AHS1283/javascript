c49c4e9ac0ccbb4e53dcfa7fc3c9d7b3b433a568
AHS1283 <aayesha.h.shaikh@gmail.com>
2024-01-05 11:22:01 +0530

    code inserted
    code inserted

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,30 @@
+console.log(`==========================Step 1===================`);
+for(var i=5;i<=15;i++)
+{
+    console.log(i);
+}
+console.log(`=========================Step 2=====================`);
+for (let index = 50; index >=40; index--) {
+    console.log(index);
+    
+}
+console.log(`=========================Step 3====================`);
+for (let index = 1; index <=30; index+=2) {
+  console.log(index);
+}
+console.log(`=========================Step 4====================`);
+for (let index = 2; index <=20; index+=2) {
+    console.log(index);
+  }
+  console.log(`=========================Step 5====================`);
+  for (let index = 5; index <=50; index+=5) {
+   console.log(index);
+}
+console.log(`=========================Step 6====================`);
+  for (let index = 1; index <=10; index++) {
+   console.log(10*index);
+}
+console.log(`=========================Step 7====================`);
+  for (let index = 10; index >=1; index--) {
+   console.log(10*index);
+}
