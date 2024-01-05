2e40d654d0d5246c291d6ed6c72355142e5e427e
AHS1283 <aayesha.h.shaikh@gmail.com>
2024-01-05 11:48:45 +0530

    code added
    code added

---
+++ 06_while_Assignment01.js
@@ -0,0 +1,46 @@
+console.log(`Using While Loop`);
+console.log(`==========================Step 1===================`);
+let i=5;
+while (i<=15) {
+    console.log(i);
+    i++;
+}
+console.log(`=========================Step 2=====================`);
+var j=50;
+while (j>=40) {
+    console.log(j);
+    j--;
+}
+console.log(`=========================Step 3====================`);
+var k=1;
+while (k<=29) {
+    console.log(k);
+    k+=2;
+}
+console.log(`=========================Step 4====================`);
+var l=2;
+while (l<=20) {
+    console.log(l);
+    l+=2;
+    
+}
+  console.log(`=========================Step 5====================`);
+  var m=1;
+while (m<=10) {
+    console.log(5*m);
+    m++;
+}
+
+console.log(`=========================Step 6====================`);
+var n=1;
+  while (n<=10) {
+    console.log(10*n);
+    n++;
+    
+  }
+console.log(`=========================Step 7====================`);
+ var p=10;
+  while (p>=1) {
+    console.log(10*p);
+    p--;
+}
